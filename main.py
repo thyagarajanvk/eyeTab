f78db57c5b76961e00964f14a4ae6d5efe81a8f1
Vinithkumar Thyagarajan <37119436+thyagarajanvk@users.noreply.github.com>
2021-10-17 09:57:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,219 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import utils, math
+import numpy as np
+import pyautogui
+import keyboard
+
+# variables 
+frame_counter =0
+CEF_COUNTER =0
+TOTAL_BLINKS =0
+# constants
+CLOSED_EYES_FRAME =3
+FONTS =cv.FONT_HERSHEY_COMPLEX
+
+
+# lips indices for Landmarks
+# Left eyes indices 
+LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+
+# right eyes indices
+RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
+
+map_face_mesh = mp.solutions.face_mesh
+# camera object 
+camera = cv.VideoCapture(0)
+# landmark detection function 
+def landmarksDetection(img, results, draw=False):
+    img_height, img_width= img.shape[:2]
+    # list[(x,y), (x,y)....]
+    mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results.multi_face_landmarks[0].landmark]
+    if draw :
+        [cv.circle(img, p, 2, (0,255,0), -1) for p in mesh_coord]
+
+    # returning the list of tuples for each landmarks 
+    return mesh_coord
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+
+# Blinking Ratio
+def blinkRatio(img, landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+    # draw lines on right eyes 
+    #cv.line(img, rh_right, rh_left, utils.GREEN, 2)
+    #cv.line(img, rv_top, rv_bottom, utils.WHITE, 2)
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom)
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom)
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio)/2
+    return ratio 
+
+# Eyes Extrctor function,
+def eyesExtractor(img, right_eye_coords, left_eye_coords):
+    # converting color image to  scale image 
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # getting the dimension of image 
+    dim = gray.shape
+
+    # creating mask from gray scale dim
+    mask = np.zeros(dim, dtype=np.uint8)
+
+    # drawing Eyes Shape on mask with white color 
+    cv.fillPoly(mask, [np.array(right_eye_coords, dtype=np.int32)], 255)
+    cv.fillPoly(mask, [np.array(left_eye_coords, dtype=np.int32)], 255)
+
+    # showing the mask 
+    # cv.imshow('mask', mask)
+    
+    # draw eyes image on mask, where white shape is 
+    eyes = cv.bitwise_and(gray, gray, mask=mask)
+    # change black color to gray other than eys 
+    # cv.imshow('eyes draw', eyes)
+    eyes[mask==0]=155
+    
+    # getting minium and maximum x and y  for right and left eyes 
+    # For Right Eye 
+    r_max_x = (max(right_eye_coords, key=lambda item: item[0]))[0]
+    r_min_x = (min(right_eye_coords, key=lambda item: item[0]))[0]
+    r_max_y = (max(right_eye_coords, key=lambda item : item[1]))[1]
+    r_min_y = (min(right_eye_coords, key=lambda item: item[1]))[1]
+
+    # For LEFT Eye
+    l_max_x = (max(left_eye_coords, key=lambda item: item[0]))[0]
+    l_min_x = (min(left_eye_coords, key=lambda item: item[0]))[0]
+    l_max_y = (max(left_eye_coords, key=lambda item : item[1]))[1]
+    l_min_y = (min(left_eye_coords, key=lambda item: item[1]))[1]
+
+    # croping the eyes from mask 
+    cropped_right = eyes[r_min_y: r_max_y, r_min_x: r_max_x]
+    cropped_left = eyes[l_min_y: l_max_y, l_min_x: l_max_x]
+
+    # returning the cropped eyes 
+    return cropped_right, cropped_left
+
+# Eyes Postion Estimator 
+def positionEstimator(cropped_eye):
+    # getting height and width of eye 
+    h, w =cropped_eye.shape
+    
+    # remove the noise from images
+    gaussain_blur = cv.GaussianBlur(cropped_eye, (9,9),0)
+    median_blur = cv.medianBlur(gaussain_blur, 3)
+
+    # applying thrsholding to convert binary_image
+    ret, threshed_eye = cv.threshold(median_blur, 50, 255, cv.THRESH_BINARY)
+
+    half = int(w/2)
+    # slicing the eyes into two parts
+    r_piece = threshed_eye[0:h, int(half-half/2): half]
+    l_piece = threshed_eye[0:h, half: int(half+half/2)]
+   
+
+    cv.imshow('right', threshed_eye)
+    cv.line(threshed_eye, (int(half-half/2), h-20), (int(half-half/2), h+20), utils.WHITE, 1)
+    cv.line(threshed_eye, (int(half+half/2), h-20), (int(half+half/2), h+20), utils.WHITE, 1)
+    
+    if np.sum(r_piece==0) > np.sum(l_piece==0):
+        eye_position = 'right'
+        switchTabsToRight = True
+    else:
+        eye_position = 'left'
+        switchTabsToRight = False
+
+    return eye_position, switchTabsToRight
+
+with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh:
+
+    # starting Video loop here.
+    while keyboard.read_key() == "p":
+        frame_counter +=1 # frame counter
+        ret, frame = camera.read() # getting frame from camera 
+        if not ret: 
+            break # no more frames break
+        #  resizing frame
+        
+        frame = cv.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv.INTER_CUBIC)
+        frame_height, frame_width= frame.shape[:2]
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        results  = face_mesh.process(rgb_frame)
+        if results.multi_face_landmarks:
+            mesh_coords = landmarksDetection(frame, results, False)
+            ratio = blinkRatio(frame, mesh_coords, RIGHT_EYE, LEFT_EYE)
+            #utils.colorBackgroundText(frame,  f'Ratio : {round(ratio,2)}', FONTS, 0.7, (30,100),2, utils.PINK, utils.YELLOW)
+
+            if ratio >4:
+                CEF_COUNTER +=1
+                #utils.colorBackgroundText(frame,  f'Blink', FONTS, 1.7, (int(frame_height/2), 100), 2, utils.YELLOW, pad_x=6, pad_y=6, )
+
+            else:
+                if CEF_COUNTER>CLOSED_EYES_FRAME:
+                    TOTAL_BLINKS +=1
+                    CEF_COUNTER =0
+            # cv.putText(frame, f'Total Blinks: {TOTAL_BLINKS}', (100, 150), FONTS, 0.6, utils.GREEN, 2)
+            #utils.colorBackgroundText(frame,  f'Total Blinks: {TOTAL_BLINKS}', FONTS, 0.7, (30,150),2)
+            
+            cv.polylines(frame,  [np.array([mesh_coords[p] for p in LEFT_EYE ], dtype=np.int32)], True, utils.GREEN, 1, cv.LINE_AA)
+            cv.polylines(frame,  [np.array([mesh_coords[p] for p in RIGHT_EYE ], dtype=np.int32)], True, utils.GREEN, 1, cv.LINE_AA)
+
+            # Blink Detector Counter Completed
+            right_coords = [mesh_coords[p] for p in RIGHT_EYE]
+            left_coords = [mesh_coords[p] for p in LEFT_EYE]
+            crop_right, crop_left = eyesExtractor(frame, right_coords, left_coords)
+            cv.imshow('right', crop_right)
+
+            debugMessage, switch = positionEstimator(crop_right)
+
+            if switch:
+                pyautogui.keyDown('alt')
+                time.sleep(.2)
+                pyautogui.press('tab')
+                time.sleep(.2)
+                pyautogui.keyUp('alt')
+                break
+            else:
+                pyautogui.keyDown('alt')
+                time.sleep(.2)
+                pyautogui.press('tab')
+                time.sleep(.2)
+                pyautogui.press('tab')
+                time.sleep(.2)
+                pyautogui.keyUp('alt')
+                break
+
+            print(debugMessage)
+            print()
+        
+        cv.imshow('frame', frame)
+    
+    cv.destroyAllWindows()
+    camera.release()
